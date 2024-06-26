846f83d15f8d13a4bad6f18008ebd4fae9c37cac
Hy <hyhuynh222@gmail.com>
2024-06-26 10:42:27 -0700

    Added Functionality of Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegexService } from './regex.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'day3-calculator';
+  formula:string = '';
+  result:string = '0';
+  srv;
+
+  constructor(srv: RegexService){
+    this.srv = srv;
+  }
+
+  reset(){
+    this.result = '0';
+    this.formula = '';
+  }
+
+  add(number:string){
+    if (this.result != '0'){
+      if(number.match(this.srv.getAll())){
+        this.formula = this.result + " ";
+      } 
+      this.result = '0';
+    }
+    this.formula += number;
+  }
+
+  opperator(opp:string){
+    if (this.result != '0'){
+      if(opp.match(this.srv.getAll())){
+        this.formula = this.result + opp;
+      } 
+      this.result = '0';
+    } else {
+      if(!this.formula.charAt(this.formula.length-1).match(this.srv.getAll())){
+        this.formula += opp;
+      } else {
+        if (this.srv.endWithSubtract().test(this.formula)){
+          this.formula = this.formula.slice(0,-2) + opp;
+        }
+        else if (this.srv.endWithAll().test(this.formula) && opp == "-" ){
+          this.formula += opp;
+        }
+        else {
+          this.formula = this.formula.slice(0,-1) + opp;
+        }
+      }
+    }
+
+  }
+
+  evaluate(){
+    if (/--/.test(this.formula)){
+      this.formula = this.formula.replace(/--/g," - - ");
+    }
+    console.log(this.formula)
+    this.result = eval(this.formula).toString();
+    this.formula = "";
+  }
 }
